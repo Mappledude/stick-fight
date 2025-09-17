5d281eeeacd86c4412ca83ad8c5ef22a5a10a323
Mappledude <sablani.zane@gmail.com>
2025-09-17 18:16:38 -0400

    Add inline firebase config and enforce inline requirement

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -9,15 +9,7 @@
 })(typeof window !== 'undefined' ? window : typeof globalThis !== 'undefined' ? globalThis : this, function (global) {
   'use strict';
 
-  const INLINE_CONFIG = Object.freeze({
-    apiKey: 'AIzaSyCTrS0i1Xz9Ll9cSPYnS3sh2g6Pfm7eNcQ',
-    authDomain: 'stick-fight-pigeon.firebaseapp.com',
-    projectId: 'stick-fight-pigeon',
-    storageBucket: 'stick-fight-pigeon.appspot.com',
-    messagingSenderId: '1035698723456',
-    appId: '1:1035698723456:web:13b6cf2b2a9f4e12a8c7b1',
-    measurementId: 'G-8X0PQR1XYZ',
-  });
+  const INLINE_CONFIG = null;
 
   const WINDOW_CONFIG_KEYS = Object.freeze([
     '__FIREBASE_CONFIG__',
@@ -138,7 +130,7 @@
     }
 
     if (!inlineConfig) {
-      logError(loggers, 'missing=config source=inline');
+      logError(loggers, 'missing=inline-config source=inline');
       throw new Error('CFG_MISSING_INLINE_CONFIG');
     }
 
